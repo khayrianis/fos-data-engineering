eaba8d548cb2bb262b02bae1831639470c63fcee
KOUDJIL Khayri Anis <koudjilkhayrianis@gmail.com>
2023-06-10 21:43:41 +0100

    the transform step in ETL process

--- transform.py
+++ transform.py
@@ -5,7 +5,9 @@ import logging
 
 # Configure logging
 logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+    filename="logfile.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
 )
 
 logging.info("We are in transforming step.")
